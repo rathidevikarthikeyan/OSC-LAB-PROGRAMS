107bbcc8b8b811fe44d4a1c2f36669f33aa2336d
Rathidevi <rathidevik2004@gmail.com>
2026-07-29 22:17:17 +0530

    second commit

---
+++ Employee/server.js
@@ -0,0 +1,68 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+app.use(express.urlencoded({
+    extended: true
+}));
+mongoose.connect('mongodb://127.0.0.1:27017/employee_db')
+.then(() => {console.log('MongoDB connected')})
+.catch((err) => {console.log( err)});
+
+const employeeschema = new mongoose.Schema({
+    empid:String,
+    name:String,
+    department:String
+});
+
+const Employee=
+    mongoose.model("Employee",employeeschema);
+
+app.get("/",(req,res)=>{
+    res.sendFile(__dirname+"/index.html");
+});
+
+app.post('/add',async(req,res)=>{
+    try{
+    await Employee.create({
+empid:req.body.empid,
+name:req.body.name,
+department:req.body.department
+    });
+   res.redirect('/view');
+}catch(err){
+    res.send(err);
+}
+    res.send("Employee added successfully");
+});
+
+app.get('/view',async(req,res)=>{
+    const employees = await Employee.find();
+    let result = `
+    <table>
+        <tr>
+            <th>Employee ID</th>
+            <th>Name</th>
+            <th>Department</th>
+            <th>Actions</th>
+            
+
+        </tr>`;
+    employees.forEach(employee => {
+        result += `
+        <tr>
+            <td>${employee.empid}</td>
+            <td>${employee.name}</td>
+            <td>${employee.department}</td>
+            <td><a>DELETE</a></td> 
+            </tr>
+            `;
+        
+    });
+    result += `</table>`;
+    res.send(result);
+    res.result;
+});
+app.listen(3000,function(){
+    console.log('Server is running on port 3000');  
+});
